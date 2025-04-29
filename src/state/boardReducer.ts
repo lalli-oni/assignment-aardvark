e297313a89f070e8d801c7525c3ead20dcc964d0
lalli-oni <lalli.oni@gmail.com>
2025-04-29 16:31:22 +0200

    Add flip-card action

--- src/state/boardReducer.ts
+++ src/state/boardReducer.ts
@@ -3,6 +3,7 @@ import { Card } from '../models/card';
 
 type BoardAction =
 	{ type: 'reset' } |
+	{ type: 'flip-card', payload: string } |
 	{ type: 'accept-pair', payload: [number, number] } |
 	{ type: 'complete' }
 
@@ -10,6 +11,16 @@ export function boardReducer(state: Board, action: BoardAction): Board {
   switch (action.type) {
 		case 'reset':
 			throw new Error('Reset board feature not implemented.')
+		case 'flip-card': {
+			const cardIndex = state.cards.findIndex((c) => c.id === action.payload)
+			if (cardIndex === -1) throw new Error(`Can't find card ${action.payload}`)
+			const card = state.cards[cardIndex]
+			const flippedCard: Card = { ...card, visibility: card.visibility === 'hidden' ? 'revealed' : 'hidden' }
+			return {
+				...state,
+				cards: state.cards.toSpliced(cardIndex, 1, flippedCard)
+			}
+		}
 		case 'accept-pair': {
 			const [firstCard, secondCard] = getCardPair(action.payload, state.cards)
 			return {
